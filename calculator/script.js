5f8a7785e245d7194b5d1f74d555ed037e7946ed
prabin-panthi <prabinpanthi33@gmail.com>
2024-12-30 00:48:56 +0545

    calculator added

---
+++ calculator/script.js
@@ -0,0 +1,46 @@
+let buttons = document.querySelectorAll(".button");
+let screen = document.querySelector(".screen");
+let flag = false;
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        if (flag && (button.innerText >= "0" && button.innerText <= "9" || button.innerText === "00")) {
+            flag = false;
+            screen.innerText = "";
+            append(button.innerText);
+        }
+        else {
+            if (button.innerText === "AC") {
+                flag = false;
+                screen.innerText = "";
+            }
+            else if (button.innerText === "") {
+                flag = false;
+                screen.innerText = screen.innerText.slice(0, -1);
+            }
+            else if (button.innerText === "=") {
+                flag = true;
+                evaluate();
+            }
+            else {
+                flag = false;
+                append(button.innerText);
+            }
+        }
+    })
+});
+
+const append = (data) => {
+    screen.innerText += data;
+}
+
+const evaluate = () => {
+    let expression = screen.innerText.replaceAll('×', '*');
+    try {
+        screen.innerText = eval(expression);
+    }
+
+    catch {
+        screen.innerText = "Syntax Error";
+    }
+}
