051a81b0ef685e919ea08b22058a938a3cf6dd90
prabin-panthi <prabinpanthi33@gmail.com>
2024-12-30 19:49:39 +0545

    added sound and modified screen

--- calculator/script.js
+++ calculator/script.js
@@ -1,9 +1,11 @@
 let buttons = document.querySelectorAll(".button");
 let screen = document.querySelector(".screen");
+const backgroundMusic = document.getElementById("backgroundMusic");
 let flag = false;
 
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
+        backgroundMusic.play();
         if (flag && (button.innerText >= "0" && button.innerText <= "9" || button.innerText === "00")) {
             flag = false;
             screen.innerText = "";
@@ -37,7 +39,7 @@ const append = (data) => {
 const evaluate = () => {
     let expression = screen.innerText.replaceAll('×', '*');
     try {
-        screen.innerText = eval(expression);
+        screen.innerText = eval(expression).toFixed(2);
     }
 
     catch {
